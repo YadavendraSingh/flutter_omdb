1bcd8fec90fdb1aff0d13ddf14342a7c7704216f
Yadavendra <yadavendra.yaduvanshi@gmail.com>
2020-05-27 18:50:54 +0530

    validations and final touches

--- lib/movie.dart
+++ lib/movie.dart
@@ -1,19 +1,19 @@
 
-class Movie{
+class Movie {
   final String title;
   final String year;
   final String imdbRating;
   final String imdbID;
   final String poster;
 
-  Movie({this.title, this.year, this.imdbRating,this.imdbID, this.poster});
+  Movie({this.title, this.year, this.imdbRating, this.imdbID, this.poster});
 
   factory Movie.fromJson(Map<String, dynamic> json) {
-    return Movie(title: json['Title'],
+    return Movie(
+        title: json['Title'],
         year: json['Year'],
         poster: json['Poster'],
         imdbRating: json['imdbRating'],
         imdbID: json['imdbID']);
   }
-
 }
